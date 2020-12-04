65c8fcfdade861f4ba26047796b7cb2327f6466a
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-05 00:50:42 +0900

    Added the optionhouseapp to the installed apps

--- OptionHouse/settings.py
+++ OptionHouse/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'optionhouseapp.apps.OptionhouseappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
