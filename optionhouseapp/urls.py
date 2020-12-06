ee33340fc8a5e9caf82af915d18afc2c09c16d53
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-06 19:00:38 +0900

    Added the url path for the post detail

--- optionhouseapp/urls.py
+++ optionhouseapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexListView.as_view(), name='index'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail')
 ]
