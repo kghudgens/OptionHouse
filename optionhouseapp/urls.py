21f156b3595a214ac77c584a5c070633d7a3a1a2
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-06 19:21:39 +0900

    Added the path for the post list

--- optionhouseapp/urls.py
+++ optionhouseapp/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexListView.as_view(), name='index'),
+    path('postlist/', views.PostListView.as_view(), name='post-list' ),
     path('about/', views.about, name='about'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail')
+
 ]
