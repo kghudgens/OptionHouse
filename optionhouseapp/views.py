46841ae6704c42cf3ea04fdf21e03b5733ab107e
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-06 18:30:24 +0900

    Created the IndexListView for the home page

--- optionhouseapp/views.py
+++ optionhouseapp/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def index(request):
     return render(request, 'optionhouseapp/index.html')
+
+class IndexListView(ListView):
+    model = Post
+    template_name = 'optionhouseapp/index.html'    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["now"] = timezone.now()
+        return context
+    
+    
