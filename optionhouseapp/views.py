523fc9cf36a46c21ac82a94c13ffdc66f287f6a1
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-06 19:01:02 +0900

    Added the detail view for the posts

--- optionhouseapp/views.py
+++ optionhouseapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.utils import timezone
 from .models import Post
 
@@ -12,11 +12,19 @@ class IndexListView(ListView):
     model = Post
     # Specified the html to display the list on 
     template_name = 'optionhouseapp/index.html'    
+    # Limits the list to show the three most recent posts 
+    queryset = Post.objects.order_by('-date')[:3]
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["now"] = timezone.now()
         return context
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name= 'optionhouseapp/post-detail.html'
+
+
     
 def about(request):
     return render(request, 'optionhouseapp/about.html')
