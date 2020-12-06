c87eb5161ce2f30d47c0a39b20c7d99856c99968
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-06 11:04:42 +0900

    Connected the postgres database

--- OptionHouse/settings.py
+++ OptionHouse/settings.py
@@ -74,10 +74,14 @@ WSGI_APPLICATION = 'OptionHouse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#Connect postgresql database to the optionhouse project
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'optionhouse',
+        'USER': 'postgres',
+        'PASSWORD':'gonzalo2727',
+        'HOST': 'localhost'
     }
 }
 
