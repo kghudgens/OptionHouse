8810953cf15417f80cea6343853e685d93f614a5
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-06 18:39:19 +0900

    added the view for the about page

--- optionhouseapp/views.py
+++ optionhouseapp/views.py
@@ -8,7 +8,9 @@ def index(request):
     return render(request, 'optionhouseapp/index.html')
 
 class IndexListView(ListView):
+    # Associated the list view with the post view
     model = Post
+    # Specified the html to display the list on 
     template_name = 'optionhouseapp/index.html'    
 
     def get_context_data(self, **kwargs):
@@ -16,4 +18,5 @@ class IndexListView(ListView):
         context["now"] = timezone.now()
         return context
     
-    
+def about(request):
+    return render(request, 'optionhouseapp/about.html')
