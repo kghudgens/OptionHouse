bb45c41d80b9a2b2d23621dd2cf15ed4b2a87606
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-06 11:46:06 +0900

    Made edits to the fields in the post model

--- optionhouseapp/models.py
+++ optionhouseapp/models.py
@@ -4,6 +4,9 @@ from PIL import Image
 class Post(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.CharField(max_length=1000)
+    content = models.TextField(max_length=1000)
     date  = models.DateField(auto_now=True)
-    image = models.ImageField(upload_to='media')
+    image = models.ImageField(upload_to='media', blank=True)
+
+    def __str__(self):
+        return f'{self.title}'
