82f73939c5fef4a1f0e45e0b029fada017abacbf
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-06 11:03:30 +0900

    Created Post model

--- optionhouseapp/models.py
+++ optionhouseapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+from PIL import Image
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=1000)
+    date  = models.DateField(auto_now=True)
+    image = models.ImageField(upload_to='media')
