cb063b234601ae5605788f15fadfbe8893feadf8
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-07 22:47:25 +0900

    Created views for the register and login path

--- User/views.py
+++ User/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from .forms import UserRegistrationForm
 
-# Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'User/register.html', {'form':form} )
+
+def login(request):
+    return render(request, 'User/login.html')
