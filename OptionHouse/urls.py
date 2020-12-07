73a4fa42a70acaaf5fcc8db3bcd34cb011769c1d
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-07 22:46:18 +0900

    Added the url paths for register and login

--- OptionHouse/urls.py
+++ OptionHouse/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from User import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('optionhouseapp.urls'))
+    path('', include('optionhouseapp.urls')),
+    path('register/', user_views.register, name='register'),
+    path('login', user_views.login, name='login')
 ]
