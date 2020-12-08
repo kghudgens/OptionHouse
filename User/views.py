0028ffcebe453e7ff65ebb07df17f0dc1676d48c
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-08 20:16:40 +0900

    Added success message to the code, fixed the redirect route for the register view

--- User/views.py
+++ User/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, redirect 
-from .forms import UserRegistrationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages 
 
 def register(request):
     if request.method == "POST":
-        form = UserRegistrationForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('index')
+            messages.success(request, 'Your account has been created, please log in.')
+            return redirect('login')
     else:
-        form = UserRegistrationForm()
+        form = UserCreationForm()
     return render(request, 'User/register.html', {'form':form} )
 
 def login(request):
