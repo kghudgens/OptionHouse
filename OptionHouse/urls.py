c9164962f1ecf479299558545749fa21877c6871
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-11 16:26:26 +0900

    added paths for log in and out

--- OptionHouse/urls.py
+++ OptionHouse/urls.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from User import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('optionhouseapp.urls')),
     path('register/', user_views.register, name='register'),
-    path('login', user_views.login, name='login')
+    path(
+        'login',
+        auth_views.LoginView.as_view(template_name='User/login.html'), 
+        name='login'),
+    path(
+        'logout', 
+        auth_views.LogoutView.as_view(template_name='User/logout.html'), 
+        name='logout'),
 ]
