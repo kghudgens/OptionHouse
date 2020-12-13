58ba2d6bc5ec446f192e08f6f4dcdcf15f2e9516
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-13 18:20:40 +0900

    imported os, added logic for the images

--- OptionHouse/settings.py
+++ OptionHouse/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,3 +129,6 @@ STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'uni_form'
 
 LOGIN_REDIRECT_URL = 'index'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
