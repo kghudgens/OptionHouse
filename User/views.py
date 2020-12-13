6b8c5e8bf78bd7964cad0ebd14a5f966b7abab7e
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-13 19:35:33 +0900

    Created view for the profile

--- User/views.py
+++ User/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, redirect 
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import DetailView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.contrib import messages 
+from .forms import *
 
 def register(request):
     if request.method == "POST":
@@ -13,5 +16,26 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'User/register.html', {'form':form} )
 
-def login(request):
-    return render(request, 'User/login.html')
+
+def profile(request):
+    u_form = UserUpdateForm(instance=request.user)
+    p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    if request.method == ("POST"):
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+
+    context = {
+        'u_form':u_form,
+        'p_form':p_form
+    }
+    return render(request, 'User/profile.html', context)
