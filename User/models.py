4d30a06a196c901c23f89de9456ee7113d6213c3
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-13 19:35:09 +0900

    Created logic to resize image

--- User/models.py
+++ User/models.py
@@ -8,3 +8,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width >300:
+            output_size = (output_size)
+            img.save(self.image.path)
