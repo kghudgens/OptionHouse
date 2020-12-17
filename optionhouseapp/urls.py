2619a42d1749718f412fcfe94ff6db8a49d41e15
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-17 22:18:32 +0900

    Added the Create post url

--- optionhouseapp/urls.py
+++ optionhouseapp/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.IndexListView.as_view(), name='index'),
     path('postlist/', views.PostListView.as_view(), name='post-list' ),
     path('about/', views.about, name='about'),
-    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail')
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post-create', views.CreatePostView.as_view(), name='post_form')
 
 ]
