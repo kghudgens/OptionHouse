7eb365a6e7794d7d17fa8e455d9d30f698649857
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-19 15:57:10 +0900

    Created the path for the update view

--- optionhouseapp/urls.py
+++ optionhouseapp/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('postlist/', views.PostListView.as_view(), name='post-list' ),
     path('about/', views.about, name='about'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path('post-create', views.CreatePostView.as_view(), name='post_form')
+    path('post-create', views.CreatePostView.as_view(), name='post_form'),
+    path('post/<int:pk>/update', views.UpdatePostView.as_view(), name='post-update' )
 
 ]
