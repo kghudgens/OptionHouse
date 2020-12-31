0f629696aaa5e23a4bfc81eb009c0f341590aec1
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-31 20:18:33 +0900

    changed crispy template to bootstrap

--- OptionHouse/settings.py
+++ OptionHouse/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIR = (
     os.path.join(BASE_DIR, 'static')
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-CRISPY_TEMPLATE_PACK = 'uni_form'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'index'
 
