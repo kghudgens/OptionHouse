8296c67e1cc632ef8a0a39ebdedc85e92cab16fa
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-31 20:19:00 +0900

    took limit off of text field

--- optionhouseapp/models.py
+++ optionhouseapp/models.py
@@ -5,7 +5,7 @@ from PIL import Image
 class Post(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField(max_length=1000)
+    content = models.TextField()
     date  = models.DateField(auto_now=True)
     image = models.ImageField(upload_to='media', blank=True)
 
